d38fe29ad4f618c5e9e612698c5fc83e2f175c73
JMS55 <47158642+JMS55@users.noreply.github.com>
2024-01-02 19:31:04 -0800

    Unload render assets from RAM (#10520)
    - No point in keeping Meshes/Images in RAM once they're going to be sent
    to the GPU, and kept in VRAM. This saves a _significant_ amount of
    memory (several GBs) on scenes like bistro.
    - References
      - https://github.com/bevyengine/bevy/pull/1782
      - https://github.com/bevyengine/bevy/pull/8624
    
    - Augment RenderAsset with the capability to unload the underlying asset
    after extracting to the render world.
    - Mesh/Image now have a cpu_persistent_access field. If this field is
    RenderAssetPersistencePolicy::Unload, the asset will be unloaded from
    Assets<T>.
    - A new AssetEvent is sent upon dropping the last strong handle for the
    asset, which signals to the RenderAsset to remove the GPU version of the
    asset.
    
    ---
    
    - Added `AssetEvent::NoLongerUsed` and
    `AssetEvent::is_no_longer_used()`. This event is sent when the last
    strong handle of an asset is dropped.
    - Rewrote the API for `RenderAsset` to allow for unloading the asset
    data from the CPU.
    - Added `RenderAssetPersistencePolicy`.
    - Added `Mesh::cpu_persistent_access` for memory savings when the asset
    is not needed except for on the GPU.
    - Added `Image::cpu_persistent_access` for memory savings when the asset
    is not needed except for on the GPU.
    - Added `ImageLoaderSettings::cpu_persistent_access`.
    - Added `ExrTextureLoaderSettings`.
    - Added `HdrTextureLoaderSettings`.
    
    - Asset loaders (GLTF, etc) now load meshes and textures without
    `cpu_persistent_access`. These assets will be removed from
    `Assets<Mesh>` and `Assets<Image>` once `RenderAssets<Mesh>` and
    `RenderAssets<Image>` contain the GPU versions of these assets, in order
    to reduce memory usage. If you require access to the asset data from the
    CPU in future frames after the GLTF asset has been loaded, modify all
    dependent `Mesh` and `Image` assets and set `cpu_persistent_access` to
    `RenderAssetPersistencePolicy::Keep`.
    - `Mesh` now requires a new `cpu_persistent_access` field. Set it to
    `RenderAssetPersistencePolicy::Keep` to mimic the previous behavior.
    - `Image` now requires a new `cpu_persistent_access` field. Set it to
    `RenderAssetPersistencePolicy::Keep` to mimic the previous behavior.
    - `MorphTargetImage::new()` now requires a new `cpu_persistent_access`
    parameter. Set it to `RenderAssetPersistencePolicy::Keep` to mimic the
    previous behavior.
    - `DynamicTextureAtlasBuilder::add_texture()` now requires that the
    `TextureAtlas` you pass has an `Image` with `cpu_persistent_access:
    RenderAssetPersistencePolicy::Keep`. Ensure you construct the image
    properly for the texture atlas.
    - The `RenderAsset` trait has significantly changed, and requires
    adapting your existing implementations.
      - The trait now requires `Clone`.
    - The `ExtractedAsset` associated type has been removed (the type itself
    is now extracted).
      - The signature of `prepare_asset()` is slightly different
    - A new `persistence_policy()` method is now required (return
    RenderAssetPersistencePolicy::Unload to match the previous behavior).
    - Match on the new `NoLongerUsed` variant for exhaustive matches of
    `AssetEvent`.

--- crates/bevy_ui/src/render/ui_material_pipeline.rs
+++ crates/bevy_ui/src/render/ui_material_pipeline.rs
@@ -612,6 +612,7 @@ pub fn extract_ui_materials<M: UiMaterial>(
     let mut changed_assets = HashSet::default();
     let mut removed = Vec::new();
     for event in events.read() {
+        #[allow(clippy::match_same_arms)]
         match event {
             AssetEvent::Added { id } | AssetEvent::Modified { id } => {
                 changed_assets.insert(*id);
@@ -620,8 +621,9 @@ pub fn extract_ui_materials<M: UiMaterial>(
                 changed_assets.remove(id);
                 removed.push(*id);
             }
+            AssetEvent::Unused { .. } => {}
             AssetEvent::LoadedWithDependencies { .. } => {
-                // not implemented
+                // TODO: handle this
             }
         }
     }
