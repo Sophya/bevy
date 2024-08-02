62e64511a4237800eb8dcaccda6e0ab5b060ec94
Pietro <pietro@sophya.ai>
2024-08-02 11:11:25 +0200

    fix: added tests

--- crates/bevy_winit/src/winit_event_filter.rs
+++ crates/bevy_winit/src/winit_event_filter.rs
@@ -2,9 +2,9 @@ use winit::event::Event as WinitEvent;
 
 use bevy_ecs::prelude::*;
 
-use crate::UpdateMode;
+use crate::{UpdateMode, WakeUp};
 
-pub struct WinitEventFilter<T: Event> {
+pub struct WinitEventFilter<T: Event = WakeUp> {
     pub filter: Option<Box<dyn Fn(&WinitEvent<T>, UpdateMode) -> bool + Send + 'static>>,
 }
 
@@ -22,10 +22,6 @@ impl<T: Event> WinitEventFilter<T> {
     }
 
     pub fn handle(&self, event: &WinitEvent<T>, update_mode: UpdateMode) -> bool {
-        let Some(filter) = &self.filter else {
-            return false;
-        };
-
         let can_react = match update_mode {
             UpdateMode::Continuous => true,
             UpdateMode::Reactive {
@@ -46,6 +42,46 @@ impl<T: Event> WinitEventFilter<T> {
             return false;
         }
 
+        let Some(filter) = &self.filter else {
+            return true;
+        };
+
         filter(event, update_mode)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+    use super::*;
+
+    #[test]
+    fn test_default() {
+        let filter = WinitEventFilter::default();
+
+        assert!(filter.handle(&WinitEvent::UserEvent(WakeUp), UpdateMode::Continuous));
+    }
+
+    #[test]
+    fn test_filter() {
+        let filter = WinitEventFilter::new(|e, _| match e {
+            WinitEvent::UserEvent(e) => false,
+            _ => true
+        });
+
+        assert!(!filter.handle(&WinitEvent::UserEvent(WakeUp), UpdateMode::Continuous));
+    }
+
+    #[test]
+    fn test_update_mode() {
+        let filter = WinitEventFilter::default();
+        let update_mode = UpdateMode::Reactive {
+            wait: Duration::MAX,
+            react_to_device_events: false,
+            react_to_user_events: false,
+            react_to_window_events: false,
+        };
+
+        assert!(!filter.handle(&WinitEvent::UserEvent(WakeUp), update_mode));
     }
 }
